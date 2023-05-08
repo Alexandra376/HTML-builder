6dc4407d33af3a9f40be0f61b74d11d5df2feb12
Alexandra <alexandra@MacBook-Air-Alexandra.local>
2023-05-08 23:53:30 +0400

    feat: solve fourth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+const filesDir = path.join(__dirname, 'files');
+const filesCopyDir = path.join(__dirname, 'files-copy');
+
+async function main() {
+    try {
+        await fs.mkdir(filesCopyDir, {recursive: true })
+        const filesCopy = await fs.readdir(filesCopyDir)
+        for (const file of filesCopy) {
+          await fs.rm(path.join(__dirname, 'files-copy', file), {force: true })
+        }
+        const filesForCopy = await fs.readdir(filesDir)
+        for (const file of filesForCopy) {
+          await fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file))
+        }
+      } catch (err) {
+        console.error(err)
+      }
+}
+main();
 
